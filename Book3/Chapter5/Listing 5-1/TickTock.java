0a9dd3728f632d01ad79ad6afb20450234803347
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-01-15 09:42:19 +0200

    22nd commit

--- Book3/Chapter5/Listing 5-1/TickTock.java
+++ Book3/Chapter5/Listing 5-1/TickTock.java
@@ -6,17 +6,21 @@ public class TickTock
     {
         // create a timer that calls the Ticker class
         // at one second intervals
+
         Timer t = new Timer(1000, new Ticker());
         t.start();
         // display a message box to prevent the
         // program from ending immediately
+
         JOptionPane.showMessageDialog(null,
             "Click OK to exit program");
     }
 }
+
 class Ticker implements ActionListener
 {
     private boolean tick = true;
+    
     public void actionPerformed(ActionEvent event)
     {
         if (tick)
