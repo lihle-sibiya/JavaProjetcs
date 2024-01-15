384618edbde22d28e8edc7b8dcfa5f54401a3a68
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-01-15 09:35:22 +0200

    21st commit

--- JavaProjects2/src/Book1/Chapter2/HelloApp.java
+++ JavaProjects2/src/Book1/Chapter2/HelloApp.java
@@ -1,5 +1,6 @@
-public class HelloApp {
-    public static void main(String[] args) {
-        //System.out.println("Hello, World!");
-    }
-}
+//public class HelloApp {
+//    public static void main(String[] args) {
+//        String greet = "Hello, World!";
+//        System.out.println(greet);
+//    }
+//}
--- JavaProjects2/src/Book1/Chapter3/HelloApp.java
+++ JavaProjects2/src/Book1/Chapter3/HelloApp.java
@@ -1,5 +1,6 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String greet = "Hello, World!";
+        System.out.println(greet);
     }
 }
