0d1234c2e3b340afa2a97791b1affedd7c32dde1
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-01-18 01:32:23 +0200

    41st commit

--- JavaProjects2/src/Book4/Chapter6/Spell.java
+++ JavaProjects2/src/Book4/Chapter6/Spell.java
@@ -1,6 +1,25 @@
 package Book4.Chapter6;
 
+import java.util.ArrayList;
+
 public class Spell {
+
+    public static void main(String[] args) {
+        // Create the ArrayList and add spells
+        ArrayList<Spell> spells = new ArrayList<>();
+        spells.add(new Spell("Aparecium", Spell.SpellType.SPELL,
+                "Makes invisible ink appear."));
+        spells.add(new Spell("Avis", Spell.SpellType.SPELL,
+                "Launches birds from your wand."));
+        spells.add(new Spell("Engorgio", Spell.SpellType.CHARM,
+                "Enlarges something."));
+        spells.add(new Spell("Fidelius", Spell.SpellType.CHARM,
+                "Hides a secret within someone."));
+        spells.add(new Spell("Finite Incatatum", Spell.SpellType.SPELL,
+                "Stops all current spells."));
+        spells.add(new Spell("Locomotor Mortis", Spell.SpellType.CURSE,
+                "Locks an opponent's legs."));
+    }
 
     public String name;
     public SpellType type;
@@ -10,13 +29,15 @@ public class Spell {
         SPELL, CHARM, CURSE
     }
 
-    public Spell(String spellName, SpellType spellType,
-            String spellDescription) {
+    // Constructor that lets you specify the name, type, and description for the
+    // spell
+    public Spell(String spellName, SpellType spellType, String spellDescription) {
         name = spellName;
         type = spellType;
         description = spellDescription;
     }
 
+    // toString() method is overridden to return simply the spell name
     public String toString() {
         return name;
     }
