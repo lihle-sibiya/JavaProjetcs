0f8a04c1b1c45306ad8f1557ffc718801a6346db
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-01-17 23:58:01 +0200

    34rd commit

--- JavaProjects2/src/Book4/Chapter5/Listing 5-1/GenStack.java
+++ JavaProjects2/src/Book4/Chapter5/Listing 5-1/GenStack.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class GenStack<E>	//formal type parameter <E>
 {
-    private LinkedList<E> list = new LinkedList<E>();	
+    private LinkedList<E> list = new LinkedList<E>();	//private LinkedList object list : keep items stored in stack
 
     public void push(E item)	
     {
-        list.addFirst(item);
+        list.addFirst(item); //addFirst method to add the item to the beginning of the list
     }
 
     public E pop()	
