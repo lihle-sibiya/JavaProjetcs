521a1f57eb31844b36e3672e0f551964b2b99539
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-01-18 00:05:56 +0200

    35th commit

--- JavaProjects2/src/Book4/Chapter5/Listing 5-1/GenStack.java
+++ JavaProjects2/src/Book4/Chapter5/Listing 5-1/GenStack.java
@@ -4,28 +4,28 @@ public class GenStack<E>	//formal type parameter <E>
 {
     private LinkedList<E> list = new LinkedList<E>();	//private LinkedList object list : keep items stored in stack
 
-    public void push(E item)	
+    public void push(E item) //parameter of type E	
     {
         list.addFirst(item); //addFirst method to add the item to the beginning of the list
     }
 
-    public E pop()	
+    public E pop()	//returns a value of type E
     {
-        return list.poll();
+        return list.poll(); //removes and returns the first element in the linked list
     }
 
-    public E peek()	
+    public E peek()	//returns a value of type E
     {
-        return list.peek();
+        return list.peek(); // returns result of the linked list’s peek method
     }
 
     public boolean hasItems()	
     {
-        return !list.isEmpty();
+        return !list.isEmpty(); //returns the opposite of the linked list’s isEmpty
     }
 
     public int size()	
     {
-        return list.size();
+        return list.size(); // returns the result of the linked list’s size method
     }
 }
