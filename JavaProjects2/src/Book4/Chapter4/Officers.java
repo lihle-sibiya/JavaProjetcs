d82f7b34ea0a493ef6c9b47bd967d3578508d16b
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-01-18 04:39:52 +0200

    46th commit

---
+++ JavaProjects2/src/Book4/Chapter4/Officers.java
@@ -0,0 +1,72 @@
+package Book4.Chapter4;
+
+import java.util.*;
+
+public class Officers {
+    public static void main(String[] args) {
+
+        // Create LinkedList
+        LinkedList<String> officers = new LinkedList<String>();
+        // Adding Items
+        officers.add("Blake");
+        officers.add("Burns");
+        officers.add("Houlihan");
+        officers.add("Pierce");
+        officers.add("McIntyre");
+
+        for (String s : officers)
+            System.out.println(s);
+
+        System.out
+                .println("Use   addFirst method                                               use   addFirst method ");
+        // addFirst method adds items to the front of the list in reverse order
+        officers.addFirst("Blake");
+        officers.addFirst("Burns");
+        officers.addFirst("Houlihan");
+        officers.addFirst("Pierce");
+        officers.addFirst("McIntyre");
+
+        for (String s : officers)
+            System.out.println(s);
+
+        System.out
+                .println("Add: specifiy the index                                        Add: specifiy the index    ");
+
+        // Add: specifiy the index
+        officers.add("Blake");
+        officers.add("Burns");
+        officers.add("Houlihan");
+        officers.add("Pierce");
+        officers.add("McIntyre");
+        officers.add(2, "Tuttle");
+        for (String s : officers)
+            System.out.println(s);
+
+        System.out.println("   Updating items                                    Updating items    ");
+
+        // Updating LinkedList Items
+        // add the original officers
+        officers.add("Blake");
+        officers.add("Burns");
+        officers.add("Tuttle");
+        officers.add("Houlihan");
+        officers.add("Pierce");
+        officers.add("McIntyre");
+        System.out.println(officers);
+        // replace Tuttle with Murdock
+        officers.set(2, "Murdock");
+        System.out.println("\nTuttle is replaced:");
+        System.out.println(officers);
+
+        
+        // Removing LinkedList Items
+        // specifying index number
+        officers.remove(3);
+        System.out.println(officers);
+        // reference to the item
+        officers.remove("Houlihan");
+        System.out.println(officers);
+
+    }
+
+}
